90023200c6779b9bba60e8a7e25f19d9450fa398
akbarmaulanad22 <zulfikarwxz22@gmail.com>
2025-08-15 02:58:10 +0700

    perjelas nama route

--- internal/delivery/http/route/route.go
+++ internal/delivery/http/route/route.go
@@ -51,31 +51,40 @@ func (route *RouteConfig) SetupAuthRoute() {
 	authRouter := route.Router.PathPrefix("/").Subrouter()
 	authRouter.Use(route.AuthMiddleware)
 
+	// logout
 	authRouter.HandleFunc("/gate/auth/logout", route.UserController.Logout).Methods("POST")
+
+	// profile
 	authRouter.HandleFunc("/gate/auth/me", route.UserController.Current).Methods("GET")
 
+	// base path
 	authRouter = route.Router.PathPrefix("/api/v1/").Subrouter()
 	authRouter.Use(route.AuthMiddleware)
 
 	// master data
+
+	// cabang
 	authRouter.HandleFunc("/branches", route.BranchController.Create).Methods("POST")
 	authRouter.HandleFunc("/branches", route.BranchController.List).Methods("GET")
 	authRouter.HandleFunc("/branches/{id}", route.BranchController.Update).Methods("PUT")
 	authRouter.HandleFunc("/branches/{id}", route.BranchController.Delete).Methods("DELETE")
 	authRouter.HandleFunc("/branches/{id}", route.BranchController.Get).Methods("GET")
 
+	// jabatan/posisi
 	authRouter.HandleFunc("/roles", route.RoleController.Create).Methods("POST")
 	authRouter.HandleFunc("/roles", route.RoleController.List).Methods("GET")
 	authRouter.HandleFunc("/roles/{id}", route.RoleController.Update).Methods("PUT")
 	authRouter.HandleFunc("/roles/{id}", route.RoleController.Delete).Methods("DELETE")
 	authRouter.HandleFunc("/roles/{id}", route.RoleController.Get).Methods("GET")
 
+	// kategori
 	authRouter.HandleFunc("/categories", route.CategoryController.Create).Methods("POST")
 	authRouter.HandleFunc("/categories", route.CategoryController.List).Methods("GET")
 	authRouter.HandleFunc("/categories/{id}", route.CategoryController.Update).Methods("PUT")
 	authRouter.HandleFunc("/categories/{id}", route.CategoryController.Delete).Methods("DELETE")
 	authRouter.HandleFunc("/categories/{id}", route.CategoryController.Get).Methods("GET")
 
+	// distributor
 	authRouter.HandleFunc("/distributors", route.DistributorController.Create).Methods("POST")
 	authRouter.HandleFunc("/distributors", route.DistributorController.List).Methods("GET")
 	authRouter.HandleFunc("/distributors/{id}", route.DistributorController.Update).Methods("PUT")
@@ -83,40 +92,52 @@ func (route *RouteConfig) SetupAuthRoute() {
 	authRouter.HandleFunc("/distributors/{id}", route.DistributorController.Get).Methods("GET")
 	// end master data
 
+	// dashboard
 	authRouter.HandleFunc("/dashboard", route.DashboardController.Get).Methods("GET")
 
+	// produk
 	authRouter.HandleFunc("/products", route.ProductController.Create).Methods("POST")
 	authRouter.HandleFunc("/products", route.ProductController.List).Methods("GET")
 	authRouter.HandleFunc("/products/{sku}", route.ProductController.Update).Methods("PUT")
 	authRouter.HandleFunc("/products/{sku}", route.ProductController.Delete).Methods("DELETE")
 	authRouter.HandleFunc("/products/{sku}", route.ProductController.Get).Methods("GET")
 
+	// karyawan/pengguna aplikasi
 	authRouter.HandleFunc("/users", route.UserController.Register).Methods("POST")
 	authRouter.HandleFunc("/users", route.UserController.List).Methods("GET")
 	authRouter.HandleFunc("/users/{username}", route.UserController.Update).Methods("PUT")
 	authRouter.HandleFunc("/users/{username}", route.UserController.Delete).Methods("DELETE")
 	authRouter.HandleFunc("/users/{username}", route.UserController.Get).Methods("GET")
 
+	// ukuran produk
 	authRouter.HandleFunc("/products/{productSKU}/sizes", route.SizeController.Create).Methods("POST")
 	authRouter.HandleFunc("/products/{productSKU}/sizes", route.SizeController.List).Methods("GET")
 	authRouter.HandleFunc("/products/{productSKU}/sizes/{id}", route.SizeController.Update).Methods("PUT")
 	authRouter.HandleFunc("/products/{productSKU}/sizes/{id}", route.SizeController.Delete).Methods("DELETE")
 	authRouter.HandleFunc("/products/{productSKU}/sizes/{id}", route.SizeController.Get).Methods("GET")
 
+	// POS (order barang keluar)
 	authRouter.HandleFunc("/sales", route.SaleController.Create).Methods("POST")
 	// authRouter.HandleFunc("/sales", route.SaleController.List).Methods("GET")
 	// authRouter.HandleFunc("/sales/{code}", route.SaleController.Update).Methods("PUT")
 	// authRouter.HandleFunc("/sales/{code}", route.SaleController.Get).Methods("GET")
 
+	// POS (order barang masuk)
 	// authRouter.HandleFunc("/purchases", route.PurchaseController.Create).Methods("POST")
 	// authRouter.HandleFunc("/purchases", route.PurchaseController.List).Methods("GET")
 	// authRouter.HandleFunc("/purchases/{code}", route.PurchaseController.Update).Methods("PUT")
 	// authRouter.HandleFunc("/purchases/{code}", route.PurchaseController.Get).Methods("GET")
 
+	// laporan barang keluar [ list per barang ]
 	authRouter.HandleFunc("/sales-and-product-reports/sales", route.SaleController.ListReport).Methods("GET")
+
+	// laporan keseluruhan barang keluar per cabang
 	authRouter.HandleFunc("/sales-and-product-reports/branch-sales", route.SaleController.ListBranchSaleReport).Methods("GET")
+
+	// laporan keseluruhan barang terlaris [ bisa filter berdasarkan cabang ]
 	authRouter.HandleFunc("/sales-and-product-reports/best-selling-product", route.SaleController.ListBestSellingProduct).Methods("GET")
 
-	authRouter.HandleFunc("/financial-reports", route.FinancialReportController.List).Methods("GET")
+	// laporan keuangan
+	// authRouter.HandleFunc("/financial-reports", route.FinancialReportController.List).Methods("GET")
 
 }
